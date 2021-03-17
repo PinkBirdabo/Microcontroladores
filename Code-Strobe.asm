70b38dcd5e047c27b34b7ba2b82e0ac73eb6214f
Emilio Méndez <78065989+PinkBirdabo@users.noreply.github.com>
2021-03-16 23:42:50 -0600

    Add files via upload

---
+++ Code-Strobe.asm
@@ -0,0 +1,96 @@
+#include "p16F628a.inc"
+__CONFIG _FOSC_INTOSCCLK & _WDTE_OFF & _PWRTE_OFF & _MCLRE_OFF & _BOREN_OFF & _LVP_OFF & _CPD_OFF & _CP_OFF
+
+RES_VECT CODE 0x0000 ; processor reset vector
+GOTO START ; go to beginning of program
+
+; TODO ADD INTERRUPTS HERE IF USED
+MAIN_PROG CODE ; let linker place main program
+
+i EQU 0x30
+j EQU 0x31
+k EQU 0x32
+x EQU 0x33
+
+;Inicio de Programa 
+START
+
+    MOVLW 0x07
+    MOVWF CMCON
+    BCF STATUS, RP1
+    BSF STATUS, RP0
+    MOVLW b'00000010'
+    MOVWF TRISB
+    BCF STATUS, RP0
+    BCF PORTB,0
+    ;BCF PORTB,1	    ;RB0 = Led   -   RB1 = Botón
+    
+inicio:
+    call botonOFF
+    BTFSS TRISB,1   ;comando que comprueba que el botón NO esté presionado, sino salta la línea inmediata.
+    goto $-2
+    call botonON
+    BTFSC TRISB,1   ;comando que comprueba que el botón SÍ esté presionado, sino salta la línea inmediata.
+    goto $-2
+    goto inicio	    ;regresa al inicio del programa
+    
+botonOFF:	    ;CICLO CON BOTÓN APAGADO (250 ms total)
+    BCF PORTB,0	    ;poner el puerto B0 (bit 0 del puerto B) en 0
+    call tiempoOFF
+    BSF PORTB,0	    ;poner el puerto B0 (bit 0 del puerto B) en 1
+    call tiempoOFF    
+    goto botonOFF   ;regresar y repetir
+            
+botonON:	    ;CICLO CON BOTÓN PRENDIDO (1500 ms total)
+    BCF PORTB,0	    ;poner el puerto B0 (bit 0 del puerto B) en 0
+    call tiempoON
+    BSF PORTB,0	    ;poner el puerto B0 (bit 0 del puerto B) en 1
+    call tiempoON
+    goto botonON    ;regresar y repetir
+
+tiempoOFF:	;CICLO DE TIEMPO DE 125 ms	        
+    movlw d'7'
+    movwf i
+    xloop:	
+	nop
+	nop
+	nop
+	movlw d'252'
+	movwf j
+    yloop:
+	decfsz j,f  
+	goto yloop
+	decfsz i,f
+	goto xloop
+	movlw d'13'	
+	movwf i
+	iloop:
+	    movlw d'50' 
+	    movwf j
+	jloop:		
+	    movlw d'60' 
+	    movwf k
+	kloop:		
+	    decfsz k,f  
+	    goto kloop
+	    decfsz j,f
+	    goto jloop
+	    decfsz i,f
+	    goto iloop
+	    return
+	    
+tiempoON:	;CICLO DE TIEMPO DE 750 ms
+    nop
+    nop
+    movlw d'35'
+    movwf x
+    decfsz x,f
+    goto $-1
+    movlw d'6'
+    movwf x       
+    call tiempoOFF
+    decfsz x,f
+    goto $-2
+    return
+    
+    END
---
+++ Code_Blink_Nombre.asm
@@ -0,0 +1,82 @@
+#include "p16F628a.inc"
+__CONFIG _FOSC_INTOSCCLK & _WDTE_OFF & _PWRTE_OFF & _MCLRE_OFF & _BOREN_OFF & _LVP_OFF & _CPD_OFF & _CP_OFF
+
+RES_VECT CODE 0x0000	; processor reset vector
+    GOTO START		; go to beginning of program
+ 
+; TODO ADD INTERRUPTS HERE IF USED
+MAIN_PROG CODE		; let linker place main program
+
+;variables
+i equ 0x30
+j equ 0x31
+k equ 0x32
+
+;INICIO DE PROGRAMA
+START
+
+    MOVLW 0x07
+    MOVWF CMCON
+    
+    BCF STATUS, RP1
+    BSF STATUS, RP0
+    MOVLW b'00000000'
+    MOVWF TRISB
+    BCF STATUS, RP0
+
+inicio: 
+    bcf PORTB,0
+    call tiempoA
+    bsf PORTB,0
+    call tiempoB
+    goto inicio
+
+;LOOP DE NOMBRE
+tiempoA: 
+    MOVLW d'174'
+    MOVWF i
+    DECFSZ i
+    goto $-1
+    MOVLW d'36'
+    MOVWF i
+loopi: 
+    MOVLW d'51'
+    MOVWF j
+loopj: 
+    MOVLW d'59'
+    MOVWF k
+    DECFSZ k
+    goto $-1
+    DECFSZ j
+    goto loopj
+    DECFSZ i
+    goto loopi
+    nop
+    nop
+    return
+
+;LOOP DE APELLIDO
+tiempoB: 
+    nop
+    nop
+    MOVLW d'94'
+    MOVWF i
+    DECFSZ i
+    goto $-1
+    MOVLW d'73'
+    MOVWF i
+loopa: 
+    MOVLW d'51'
+    MOVWF j
+loopb: 
+    MOVLW d'58'
+    MOVWF k
+    DECFSZ k
+    goto $-1
+    nop
+    DECFSZ j
+    goto loopb
+    DECFSZ i
+    goto loopa
+    return
+END
